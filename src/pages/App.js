7a59f4b4d61f4d8734815be8d46573f3b093903e
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-12-23 13:05:57 +0700

    App: Minor layout changes
    Rearrange import statements.
    Rename class name 'App' as 'inner'.

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,12 @@
 import Nav from './../components/Nav';
+import Hero from './../components/Hero';
 import './css/App.css';
+
 
 function App() {
   return (
     <div className="wrapper">
-      <div className="App">
+      <div className="inner">
         <Nav />
       </div>
     </div>
