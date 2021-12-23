e74763e0568f2465350d2905ddb8cf38dc0375ab
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-12-23 23:04:43 +0700

    EmailSignup: Rename 'header' class
    Rename the 'header' class as 'learn-header'. This was causing a namespace clash with the class in <Membership/> with the same name.
    Add left margin to the 'header' class in order to align properly with the items below.

--- src/components/EmailSignup.js
+++ src/components/EmailSignup.js
@@ -5,7 +5,7 @@ import './css/EmailSignup.css';
 function EmailSignup() {
   return (
     <div className="email-signup">
-      <h2 className="header">Wanna learn stock options?</h2>
+      <h2 className="learn-header">Wanna learn stock options?</h2>
       <p className="learn-text">Get news on what you missed in the private chat, updates on new classes in the chat, and see brand new "Put Gang" members who started as beginners winning in the stock market.</p>
       <div className="email-container">
         <input placeholder="Email" name="email" type="email" id="email"/>
