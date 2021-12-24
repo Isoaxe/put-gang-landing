0ab71336a0fb5dfcca0e351d59a94265551ff117
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-12-24 23:09:48 +0700

    App: Implement LearnModal component
    + New state variable to track modal visibility.
    + Import and add the modal component and pass visibility prop and setter method.
    + Also pass setter methods to other components.

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,21 +1,28 @@
+import { useState } from 'react';
 import Nav from './../components/Nav';
 import Hero from './../components/Hero';
 import Membership from './../components/Membership';
 import EmailSignup from './../components/EmailSignup';
+import LearnModal from './../components/LearnModal';
 import './css/App.css';
 
 
 function App() {
+  const [learnModalVisible, setLearnModalVisible] = useState(false);
+
   return (
     <div className="wrapper">
       <div className="inner">
         <Nav />
       </div>
-      <Hero />
+      <Hero setLearnModalVisible={setLearnModalVisible} />
       <div className="inner">
-        <Membership />
+        <Membership setLearnModalVisible={setLearnModalVisible} />
         <EmailSignup />
       </div>
+      <LearnModal
+        learnModalVisible={learnModalVisible} setLearnModalVisible={setLearnModalVisible}
+      />
     </div>
   );
 }
