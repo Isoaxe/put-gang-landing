5c7378a6843602394915d2f16ecfb8937f49d77e
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-12-26 20:28:37 +0700

    LearnModal: Radio buttons change state
    + Add an onClick attribute to the radio buttons so that they set the learnModalChoice to the appropriate value.
    + Create two new variables to dynamically set the action and amount in the modal header.
    + Implement these variables in the header. This way when the state updates by selecting the radio buttons, the header text will update to show the selected option.

--- src/components/LearnModal.js
+++ src/components/LearnModal.js
@@ -4,6 +4,17 @@ import './css/LearnModal.css';
 
 function LearnModal(props) {
   Modal.setAppElement("#root");
+  const choice = props.learnModalChoice;
+  let action, amount;
+
+  if (choice === "watch") {
+    action = "Watch";
+    amount = "50";
+  } else
+  if (choice === "join") {
+    action = "Join";
+    amount = "150";
+  }
 
   function close() {
     props.setLearnModalVisible(false);
@@ -19,9 +30,9 @@ function LearnModal(props) {
 		>
       <div>
         <div className="modal-headlines">
-          <h2 className="modal-heading">Watch the discussion</h2>
+          <h2 className="modal-heading">{action} the discussion</h2>
           <h5 className="modal-subheading">by Put Gang</h5>
-          <h2 className="modal-heading-price">$50 / month</h2>
+          <h2 className="modal-heading-price">${amount} / month</h2>
         </div>
         <div className="modal-options-container">
           <div className="modal-option">
@@ -29,14 +40,14 @@ function LearnModal(props) {
               <p className="modal-option-name">Watch the discussion</p>
               <p className="modal-option-price">$50 / month</p>
             </label>
-            <input type="radio" name="price-options" id="watch" />
+            <input type="radio" name="price-options" id="watch" onClick={() => props.setLearnModalChoice("watch")} />
           </div>
           <div className="modal-option">
             <label for="join">
               <p className="modal-option-name">Join the discussion</p>
               <p className="modal-option-price">$150 / month</p>
             </label>
-            <input type="radio" name="price-options" id="join" />
+            <input type="radio" name="price-options" id="join" onClick={() => props.setLearnModalChoice("join")} />
           </div>
         </div>
         <div className="modal-button-container">
