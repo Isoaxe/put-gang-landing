4e5e61d404175c70ceee75e572960770ca868d24
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-12-26 16:30:32 +0700

    LearnModal: Create radio options
    Create the 'watch' and 'join' sections of the modal with the text, price and a radio button for each.
    Tidy up the css by removing redundant class names and selectors.
    Adjust modal position.

--- src/components/LearnModal.js
+++ src/components/LearnModal.js
@@ -17,13 +17,27 @@ function LearnModal(props) {
 			className="content"
 			overlayClassName="overlay"
 		>
-      <div className="learn-more">
+      <div>
         <div className="modal-headlines">
           <h2 className="modal-heading">Watch the discussion</h2>
           <h5 className="modal-subheading">by Put Gang</h5>
           <h2 className="modal-heading-price">$50 / month</h2>
         </div>
-        <div className="modal-options">
+        <div className="modal-options-container">
+          <div className="modal-option">
+            <label for="watch">
+              <p className="modal-option-name">Watch the discussion</p>
+              <p className="modal-option-price">$50 / month</p>
+            </label>
+            <input type="radio" name="price-options" id="watch" />
+          </div>
+          <div className="modal-option">
+            <label for="join">
+              <p className="modal-option-name">Join the discussion</p>
+              <p className="modal-option-price">$150 / month</p>
+            </label>
+            <input type="radio" name="price-options" id="join" />
+          </div>
         </div>
       </div>
     </Modal>
