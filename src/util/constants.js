01670819c4236da382548bece14d9a27f00d6118
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-21 15:12:41 +0700

    Create constants.js
    + Create a file to hold constants that may be used in several locations throughout the app.
    + Add the Stripe public api keys for both test and live mode here. These are not secret.

---
+++ src/util/constants.js
@@ -0,0 +1,8 @@
+/*
+ * Constants used within the app. Best to have a single point of reference.
+ */
+
+
+ // Stripe API details.
+ export const STRIPE_PUBLIC_KEY_TEST = "pk_test_K8ZJXsFTntA75GGQDTMmAZrX";
+ export const STRIPE_PUBLIC_KEY_LIVE = "pk_live_krfnainmAevhtqlIRYMNfY5Y";
