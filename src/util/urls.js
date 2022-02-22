75615a349c07e31d95dd51425a71d93b992ed59c
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-22 12:43:05 +0700

    urls: Set api and landing urls
    + Set url statically for the hosted landing page.
    + Set the api url dynamically based on whether the site is being run locally or not.
    + Update introductory comment to suit.

--- src/util/urls.js
+++ src/util/urls.js
@@ -1,6 +1,15 @@
 /*
- * Set URLs as constants to have a single point of reference.
+ * Statically set hosted URLs. Set API URL dynamically based on host.
  */
 
 
+// These are the static hosted urls.
+export const LANDING_URL = "https://put-gang-landing.web.app";
 export const CONSOLE_BASE_URL = "https://put-gang-console.web.app";
+
+
+// Set API_URL based on whether locally hosted emulator is running or not.
+const localApiUrl = "http://localhost:5001/put-gang/us-central1/api";
+const remoteApiUrl = "https://us-central1-put-gang.cloudfunctions.net/api";
+
+export const API_URL = ((window.location.hostname === "localhost") ? localApiUrl : remoteApiUrl);
