41e06f977fa757af7413cec0c0c6ba4f75e841e9
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-23 14:33:51 +0700

    Create EmailModal.js
    Create a basic modal to collect the users email address. This will be used to create a customer in Stripe.

---
+++ src/components/EmailModal.js
@@ -0,0 +1,37 @@
+import Modal from "react-modal";
+import './css/EmailModal.css';
+
+
+function EmailModal(props) {
+  Modal.setAppElement("#root");
+
+  function close() {
+    // Don't want the modal to accidentally close, so disable.
+    return;
+  }
+
+  async function continueToPayments () {
+    const secret = await createPaymentIntent(choice);
+    props.setClientSecret(secret);
+    props.setLearnModalVisible(false);
+    props.setPaymentsModalVisible(true);
+  }
+
+  return (
+    <Modal
+			isOpen={props.emailModalVisible}
+			onRequestClose={close}
+			contentLabel="Stripe Email Modal"
+			className="content"
+			overlayClassName="overlay"
+		>
+      <div>
+        <div className="modal-button-container">
+          <button onClick={continueToPayments}>Continue</button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+export default EmailModal;
