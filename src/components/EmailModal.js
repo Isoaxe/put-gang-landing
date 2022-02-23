a6cccc3cf35f09dded8de1f38abf8fb8c25b5d49
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-23 15:15:01 +0700

    EmailModal: Add choice variable
    + Previously forgot to import the createPaymentIntent function.
    + Also forgot to declare choice. It's already in use by the createPaymentIntent function.

--- src/components/EmailModal.js
+++ src/components/EmailModal.js
@@ -1,9 +1,11 @@
 import Modal from "react-modal";
+import { createPaymentIntent } from './../util/stripe';
 import './css/EmailModal.css';
 
 
 function EmailModal(props) {
   Modal.setAppElement("#root");
+  const choice = props.learnModalChoice;
 
   function close() {
     // Don't want the modal to accidentally close, so disable.
