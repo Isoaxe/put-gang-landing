30364547f4ebfcfdb0d88a22d1a3a2bbc02e4afe
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-24 19:37:38 +0700

    PaymentsModal: Add form handling
    Write a method to handle the sending of the payments form. This confirms payment and routes to the console for account creation when complete.

--- src/components/PaymentsModal.js
+++ src/components/PaymentsModal.js
@@ -1,17 +1,30 @@
 import Modal from "react-modal";
 import { PaymentElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { CONSOLE_URL } from './../util/urls';
 import './css/PaymentsModal.css';
 import './css/shared.css';
 
 
 function PaymentsModal(props) {
   Modal.setAppElement("#root");
+  const choice = props.learnModalChoice;
   const stripe = useStripe();
   const elements = useElements();
 
   function close() {
     // Don't want the modal to accidentally close, so disable.
     return;
+  }
+
+  async function handleSubmit (event) {
+    event.preventDefault();
+    if (!stripe || !elements) return;
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: `${CONSOLE_URL}/session/signup?refId=${props.referrerId}&membLvl=${choice}`,
+      },
+    });
   }
 
   return (
@@ -22,11 +35,11 @@ function PaymentsModal(props) {
 			className="content"
 			overlayClassName="overlay"
 		>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Enter card details:</h3>
         <PaymentElement />
         <div className="modal-button-container">
-          <button>Submit</button>
+          <button id="submit">Submit</button>
         </div>
       </form>
     </Modal>
