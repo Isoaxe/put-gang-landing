4cb5611fbcfbc022a744c27062ff434d1504cd5a
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-24 18:01:37 +0700

    stripe: Write createSubscription function
    Add a function that makes a call to the Put Gang server requesting the creation of a new subscription.
    On the server, the Stripe api will be called to create the subscription.

--- src/util/stripe.js
+++ src/util/stripe.js
@@ -28,3 +28,16 @@ export async function createCustomer (email) {
   const stripeUid = await response.json();
   return stripeUid;
 }
+
+
+// Create a subscription in Stripe.
+export async function createSubscription (priceId, customerId) {
+  const fetchConfig = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ priceId, customerId }),
+  };
+  const response = await fetch(`${API_URL}/stripe/subscription`, fetchConfig);
+  const jsonResponse = await response.json();
+  return jsonResponse;
+}
