3f05a8a29503b92e795c8492929f8ca7e9d8c61d
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-24 18:14:19 +0700

    stripe: Remove createPaymentIntent
    Remove the createPaymentIntent function as it's no longer needed.

--- src/util/stripe.js
+++ src/util/stripe.js
@@ -4,19 +4,6 @@
 import { API_URL } from "./urls";
 
 
-// Create a payment intent for a given membership level that returns a secret.
-export async function createPaymentIntent (membLvl) {
-  const fetchConfig = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ membLvl }),
-  };
-  const response = await fetch(`${API_URL}/stripe/payments`, fetchConfig);
-  const jsonResponse = await response.json();
-  return jsonResponse.clientSecret;
-}
-
-
 // Create a new customer in Stripe. Required for subscription payments.
 export async function createCustomer (email) {
   const fetchConfig = {
