23177d2d5ec3ae8b5e5dd9c5b010f898311d0c45
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-02-27 11:28:31 +0700

    helpers: Add disableButtonContainer
    Write a function that adds a class called 'disabled' to the wrapper element of a disabled button. Also remove the class when the button is n longer disabled.

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,3 +1,17 @@
 /*
  * Helper functions used by the put-gang-landing frontend.
  */
+
+
+// Add the 'disabled' class to the parent of a disabled button. Remove when not disabled.
+export function disableButtonContainer () {
+  const container = document.getElementsByClassName("modal-button-container")[0];
+  const button = container?.childNodes[0];
+  if (container && button) {
+    if (button.disabled) {
+      container.classList.add("disabled");
+    } else if (!button.disabled) {
+      container.classList.remove("disabled");
+    }
+  }
+}
