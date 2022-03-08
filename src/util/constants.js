7246775bfd751d51bab102f2549641ff2066f752
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-03-08 13:32:57 +0700

    constants: Roll price ids
    Wiped all the test data. Unfortunately that included the products that were created too. Had to re-create them, albeit now with different ids.

--- src/util/constants.js
+++ src/util/constants.js
@@ -6,5 +6,5 @@
 // Stripe API details.
 export const STRIPE_PUBLIC_KEY_TEST = "pk_test_K8ZJXsFTntA75GGQDTMmAZrX";
 export const STRIPE_PUBLIC_KEY_LIVE = "pk_live_krfnainmAevhtqlIRYMNfY5Y";
-export const STRIPE_WATCH_ID = "price_0KUMQZQ8oyfKvy6CgpdT1Jd2";
-export const STRIPE_JOIN_ID = "price_0KUMSzQ8oyfKvy6CR5ulFCgA";
+export const STRIPE_WATCH_ID = "price_0KawSsQ8oyfKvy6CqW2hR0uW";
+export const STRIPE_JOIN_ID = "price_0KawTrQ8oyfKvy6Cgslvo66N";
