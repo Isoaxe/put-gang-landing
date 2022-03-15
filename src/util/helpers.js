e05039ccdcaeadbcf2b6f1053381f12a0adc9a47
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-03-15 12:59:14 +0700

    helpers: Add getLinkToken function
    Add a function that makes an api request to the Put Gang server to retrieve a link token.

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,6 +1,24 @@
 /*
  * Helper functions used by the put-gang-landing frontend.
  */
+import { API_URL } from "./urls";
+
+// Request a link token for Plaid access from the server.
+export async function getLinkToken() {
+  const fetchConfig = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  };
+  const response = await fetch(
+    API_URL + "/plaid/create-link-token",
+    fetchConfig
+  );
+  const jsonResponse = await response.json();
+  return jsonResponse;
+}
 
 // Add the 'disabled' class to the parent of a disabled button. Remove when not disabled.
 export function disableButtonContainer() {
