bfa8439be58a828f081335696c2503198e805a77
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-03-15 22:21:23 +0700

    helpers: Remove getLinkToken
    Move this function to App.js.

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,24 +1,6 @@
 /*
  * Helper functions used by the put-gang-landing frontend.
  */
-import { API_URL } from "./urls";
-
-// Request a link token for Plaid access from the server.
-export async function getLinkToken() {
-  const fetchConfig = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  };
-  const response = await fetch(
-    API_URL + "/plaid/create-link-token",
-    fetchConfig
-  );
-  const jsonResponse = await response.json();
-  return jsonResponse;
-}
 
 // Add the 'disabled' class to the parent of a disabled button. Remove when not disabled.
 export function disableButtonContainer() {
